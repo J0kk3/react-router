2c367968c8ca08a93d919445a9d690a8291b47fd
J0kk3 <joakim_88@msn.com>
2022-09-13 21:48:30 +0200

    [ADD] basic structure, NavLinks

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import ReactDOM from 'react-dom/client';
-
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+//app
 import App from './App';
+//styles
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot( document.getElementById( 'root' ) );
+root.render( <BrowserRouter><App /></BrowserRouter> );
